be70ad06059b5e47adbebc7b05cc9247bbd599c8
David Vorona <davidavorona@gmail.com>
2021-10-12 12:10:54 -0700

    Implement basic web server

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const PORT = 3000;
+const gimpLocations = {};
+
+function createOrUpdateGimpLocation(data) {
+    const gimpName = data.name;
+    delete data.name;
+    if (!gimpLocations.hasOwnProperty(gimpName)) {
+        gimpLocations[gimpName] = {};
+    }
+    gimpLocations[gimpName] = {
+        x: parseInt(data.x, 10),
+        y: parseInt(data.y, 10),
+        plane: parseInt(data.plane, 10)
+    };
+}
+
+function removeGimpLocation(name) {
+    delete gimpLocations[name];
+}
+
+const app = express()
+    .use(express.urlencoded({ extended: false }))
+    .use(express.json());
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/ping", (req, res) => {
+    res.json(gimpLocations);
+});
+
+app.post("/broadcast", (req, res) => {
+    try {
+        const data = req.body;
+        if (!data) {
+            return res.status(400).json({ err: "Location data is required" });
+        }
+        createOrUpdateGimpLocation(data);
+        res.json({ success: "Updated location" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: "Failed to broadcast location" });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+});
